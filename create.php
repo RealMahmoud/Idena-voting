ad69d8b05b46d16bfd1c572c64814ee5902323c4
ProjXDev <61373609+ProjXDev@users.noreply.github.com>
2020-05-16 16:07:12 +0200

    update

---
+++ create.php
@@ -0,0 +1,50 @@
+<?php
+session_start();
+include("_config.php");
+if (!empty($_GET['token'])){
+  $_SESSION["token"] = $_GET['token'];
+}
+if (empty($_GET['id'])){
+  die("404");
+}
+if(!empty($_SESSION["token"])) {
+$sql = "SELECT * FROM `auth` WHERE `token` = '".$_SESSION["token"]."';";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    $auth   = $row['authenticated'];
+
+if ($auth == 0){
+  header("location:login.php");
+}
+}}
+;}
+if (empty($_SESSION["token"])){
+    header("location:login.php");
+}
+
+$sql = "SELECT * FROM `auth` WHERE `token` = '".$_SESSION["token"]."';";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    $addr = $row['addr'];
+
+}}
+?>
+
+
+
+<h3>Hi ,<?php echo    $addr ?></h3>
+
+<h3><?php echo  $row['pdesc'] ?></h3>
+<form action="vote.php" method="GET">
+  <input type="hidden" name="id" value="<?php echo  $row['id'] ?>" >
+<input type="hidden" name="type" value="poll" >
+Yes: <input type="radio" name="vote" value="0" checked><br>
+No: <input type="radio" name="vote" value="1" >
+<input type="submit" value="Vote">
+</form>

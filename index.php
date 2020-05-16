6c245648baa020c52eaebeb86faca5596fef1b7f
ProjXDev <61373609+ProjXDev@users.noreply.github.com>
2020-05-16 14:52:01 +0200

    Update index.php

--- index.php
+++ index.php
@@ -23,19 +23,7 @@ if (empty($_SESSION["token"])){
     header("location:login.php");
 }
 ?>
-A BIG NICE
-Idea : 1 --Sending with custom payload --- Proposal ID + Answer + Nonce
-Do you thing Idena will go to the moon?
-<form action="dna://send/v1" method="get" >
 
-<input type="hidden" id="addr" name="address" value="0xb30348813590e02907da79e1c46fba4edca5a2d8">
-   <input type="hidden" id="amount" name="amount" value="0.0000000001">
-  <input type="radio" id="vote2" name="comment" value="v=1-id=1-n=55"checked>
-  <label for="yes">Yes</label><br>
-  <input type="radio" id="vote2" name="comment" value="v=1-id=2-n=55">
-  <label for="no">NO</label><br>
-  <input type="submit" value="Submit">
-</form>
 
 
 Logout?

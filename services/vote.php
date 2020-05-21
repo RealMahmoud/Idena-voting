3f8a08942e4791907af4cf635105aed1864309bc
bingbinglee <supriyo789.next@gmail.com>
2020-05-21 11:07:56 -0400

    Add voting services

---
+++ services/vote.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+include(dirname(__FILE__)."/../common/_config.php");
+include(dirname(__FILE__)."/../common/protected.php");
+header('Content-Type: application/json');
+
+$id = $conn->real_escape_string($_POST['id']);
+$vote = $conn->real_escape_string($_POST['vote']);
+$type = $conn->real_escape_string($_POST['type']);
+
+$sql = "SELECT * FROM `votes` WHERE `addr` = '".$addr."' AND `pid` = '".$id."' LIMIT 1;";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+          // output data of each row
+          while($row = $result->fetch_assoc()) {
+          echo '{"success":false, "data": "you have already voted"}';
+          }
+} else {
+    $sql = "INSERT INTO `votes`(`pid`, `addr`, `vote`,`type`) VALUES ('".$id."','".$addr."','".$vote."','".$type."');";
+    $result = $conn->query($sql);
+    echo '{"success":true, "data": "vote casted successfully"}';
+}
+?>

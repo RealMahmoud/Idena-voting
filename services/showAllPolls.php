8af413d7f55678c0daa483b758a9443b196af50d
bingbinglee <supriyo789.next@gmail.com>
2020-05-21 00:54:30 -0400

    update services

---
+++ services/showAllPolls.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+include(dirname(__FILE__)."/../common/_config.php");
+header('Content-Type: application/json');
+$polls = array();
+$entries = array();
+?>
+<?php
+    $sql1 = "SELECT * FROM `polls`";
+    $result_acct = $conn->query($sql1);
+    if ($result_acct->num_rows > 0) {
+        // output data of each row
+                while($row = $result_acct->fetch_assoc()) {
+                    $polls[] = array('description' => $row['pdesc'],'id' => $row['id'],'addr' => $row['addr']);
+                }
+                $entries = $result = array('entries' => $polls);
+                echo json_encode($entries);
+    }
+?>

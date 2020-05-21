38ead9b276f75a938fb6c8664fd2ac042177c70b
bingbinglee <supriyo789.next@gmail.com>
2020-05-21 00:53:55 -0400

    update partials

---
+++ partials/footer.php
@@ -0,0 +1,12 @@
+  <!-- footer code begins here --->
+
+  <script src="./js/jquery-3.1.1.min.js"></script>
+  <script src="./js/popper.min.js"></script>
+  <script src="./js/bootstrap.min.js"></script>
+  <script src="./js/jquery-dateformat.min.js"></script>
+  <script src="./js/preload.js"></script>
+  <script src="./js/ajax-utils.js"></script>
+  <script src="./js/global.js"></script>
+
+</body>
+</html>

c3c33b72ebe2e73ee3dba9ff3c94a164cfc4ddad
ProjXDev <61373609+ProjXDev@users.noreply.github.com>
2020-05-24 01:59:26 +0200

    STATS

---
+++ services/option-stats.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+include(dirname(__FILE__)."/../common/_config.php");
+header('Content-Type: application/json');
+if(empty($_GET['pid'])||empty($_GET['vote'])){
+  die("Error");
+}
+$pid = $conn->real_escape_string($_GET['pid']);
+$vote = $conn->real_escape_string($_GET['vote']);
+$type = $conn->real_escape_string($_GET['type']);
+$resultjson =(object)array();
+
+
+$result = $conn->query("SELECT COUNT(*) FROM `accounts` INNER JOIN `votes` ON `accounts`.`address`=`votes`.`addr` WHERE  `vote` = '".$vote."'AND `pid` = '".$pid."' AND `type` = '".$type."';");
+$row = $result->fetch_row();
+$resultjson->All=$row[0];
+
+$result = $conn->query("SELECT COUNT(*) FROM `accounts` INNER JOIN `votes` ON `accounts`.`address`=`votes`.`addr` WHERE  `vote` = '".$vote."'AND `pid` = '".$pid."' AND `type` = '".$type."'AND`state` = 'Human';");
+$row = $result->fetch_row();
+$resultjson->Human=$row[0];
+
+$result = $conn->query("SELECT COUNT(*) FROM `accounts` INNER JOIN `votes` ON `accounts`.`address`=`votes`.`addr` WHERE  `vote` = '".$vote."'AND `pid` = '".$pid."' AND `type` = '".$type."'AND `state` = 'Verified';");
+$row = $result->fetch_row();
+$resultjson->verified=$row[0];
+
+$result = $conn->query("SELECT COUNT(*) FROM `accounts` INNER JOIN `votes` ON `accounts`.`address`=`votes`.`addr` WHERE  `vote` = '".$vote."'AND `pid` = '".$pid."' AND `type` = '".$type."'AND`state` = 'Newbie';");
+$row = $result->fetch_row();
+$resultjson->newbie=$row[0];
+
+$result = $conn->query("SELECT COUNT(*) FROM `accounts` INNER JOIN `votes` ON `accounts`.`address`=`votes`.`addr` WHERE  `vote` = '".$vote."'AND `pid` = '".$pid."' AND `type` = '".$type."' AND`state` <> 'Human' AND `state` <> 'Verified' AND `state` <> 'Newbie';");
+$row = $result->fetch_row();
+
+$resultjson->notvalidated=$row[0];
+
+if ($type == 'poll'){
+  $db = 'polls';
+}else{
+    $db = 'proposals';
+}
+$sql = "SELECT * FROM `".$db."` WHERE `id` = '".$pid."' LIMIT 1;";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  while($row = $result->fetch_assoc()) {
+    $resultjson->desc=$row['option'.$vote];
+  }
+}
+echo json_encode($resultjson);
+
+
+?>
--- services/stats.php
+++ services/stats.php
@@ -1,69 +1,52 @@
 <?php
 session_start();
 include(dirname(__FILE__)."/../common/_config.php");
-// header('Content-Type: application/json');
+header('Content-Type: application/json');
 if(empty($_GET['pid'])||empty($_GET['type'])){
   die("Error");
 }
 $type = $conn->real_escape_string($_GET['type']);
 $pid = $conn->real_escape_string($_GET['pid']);
-
+$resultjson =(object)array();
 // Count votes
 $result = $conn->query("SELECT COUNT(*) FROM `accounts` INNER JOIN `votes` ON `accounts`.`address`=`votes`.`addr` WHERE `pid` = '".$pid."' AND `type` = '".$type."';");
 $row = $result->fetch_row();
-echo '<br>AllVotesCount:'.$row[0];
-
+$resultjson->AllVotesCount=$row[0];
 $result = $conn->query("SELECT COUNT(*) FROM `accounts` INNER JOIN `votes` ON `accounts`.`address`=`votes`.`addr` WHERE `state` = 'Human' AND `pid` = '".$pid."' AND `type` = '".$type."';");
 $row = $result->fetch_row();
-echo '<br>HumansVotesCount:'.$row[0];
-
+$resultjson->HumansVotesCount=$row[0];
 $result = $conn->query("SELECT COUNT(*) FROM `accounts` INNER JOIN `votes` ON `accounts`.`address`=`votes`.`addr` WHERE `state` = 'Verified' AND `pid` = '".$pid."' AND `type` = '".$type."';");
 $row = $result->fetch_row();
-echo '<br>VerifiedVotesCount:'.$row[0];
-
+$resultjson->VerifiedVotesCount=$row[0];
 $result = $conn->query("SELECT COUNT(*) FROM `accounts` INNER JOIN `votes` ON `accounts`.`address`=`votes`.`addr` WHERE `state` = 'Newbie' AND `pid` = '".$pid."' AND `type` = '".$type."';");
 $row = $result->fetch_row();
-echo '<br>NewbieVotesCount:'.$row[0];
-
+$resultjson->NewbieVotesCount=$row[0];
 $result = $conn->query("SELECT COUNT(*) FROM `accounts` INNER JOIN `votes` ON `accounts`.`address`=`votes`.`addr` WHERE `state` <> 'Human' AND `state` <> 'Verified' AND `state` <> 'Newbie' AND `pid` = '".$pid."' AND `type` = '".$type."';");
 $row = $result->fetch_row();
-echo '<br>NoneValidatedVotesCount:'.$row[0];
+$resultjson->NoneValidatedVotesCount=$row[0];
 
 
-echo "<br>";
+
 
 //  Count accounts
 $result = $conn->query("SELECT COUNT(*) FROM `accounts`;");
 $row = $result->fetch_row();
-echo '<br>AllValidatedCount:'.$row[0];
+$resultjson->AllValidatedCount=$row[0];
 $result = $conn->query("SELECT COUNT(*) FROM `accounts` WHERE `state` = 'Human';");
 $row = $result->fetch_row();
-echo '<br>HumansVotesCount:'.$row[0];
+$resultjson->HumansCount=$row[0];
 $result = $conn->query("SELECT COUNT(*) FROM `accounts` WHERE `state` = 'Verified';");
 $row = $result->fetch_row();
-echo '<br>VerifiedCount:'.$row[0];
+$resultjson->VerifiedCount=$row[0];
 $result = $conn->query("SELECT COUNT(*) FROM `accounts` WHERE `state` = 'Newbie';");
 $row = $result->fetch_row();
-echo '<br>NewbieCount:'.$row[0];
+$resultjson->NewbieCount=$row[0];
 $result = $conn->query("SELECT COUNT(*) FROM `accounts` WHERE `state` <> 'Human' AND `state` <> 'Verified' AND `state` <> 'Newbie';");
 $row = $result->fetch_row();
-echo '<br>NoneValidatedCount:'.$row[0];
-/*
-
-$result = array("Toyota"=>"Highlander");
-
-$result2 = array("Toota"=>"Highlander");
-$result = $result + $result2;
+$resultjson->NoneValidatedCount=$row[0];
+echo json_encode($resultjson);
 
 
-
-
-
-$humans[] = $count
-
-    echo json_encode($result);
-
-*/
 
 
 

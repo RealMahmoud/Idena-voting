a0070647ff836e9aebabd1e9ff2f8026f042d05a
ProjXDev <61373609+ProjXDev@users.noreply.github.com>
2020-06-29 18:29:17 +0200

    Update cron.php

--- common/cron.php
+++ common/cron.php
@@ -17,7 +17,7 @@ $conn->query($sql);
 
 
 
-$sql2 = "SELECT * , (SELECT COUNT(*) FROM `accounts` INNER JOIN `votes` ON `accounts`.`address`=`votes`.`addr` WHERE (`state` = 'Verified' OR `state` = 'Human' OR `state` = 'Newbie') AND `type` = 'proposal' AND `pid`= `proposals`.`id` )AS 'count' FROM proposals WHERE (SELECT COUNT(*) FROM `accounts` INNER JOIN `votes` ON `accounts`.`address`=`votes`.`addr` WHERE `type` = 'proposal' AND `pid`= `proposals`.`id`) > 4 AND `ann` = '0' AND`endtime` > NOW() ORDER BY (SELECT COUNT(*) FROM `accounts` INNER JOIN `votes` ON `accounts`.`address`=`votes`.`addr` WHERE `type` = 'proposal' AND `pid`= `proposals`.`id`) DESC LIMIT 50";
+$sql2 = "SELECT * , (SELECT COUNT(*) FROM `accounts` INNER JOIN `votes` ON `accounts`.`address`=`votes`.`addr` WHERE (`state` = 'Verified' OR `state` = 'Human' OR `state` = 'Newbie') AND `type` = 'proposal' AND `pid`= `proposals`.`id` )AS 'count' FROM proposals WHERE (SELECT COUNT(*) FROM `accounts` INNER JOIN `votes` ON `accounts`.`address`=`votes`.`addr` WHERE `type` = 'proposal' AND `pid`= `proposals`.`id`) > 9 AND `ann` = '0' AND`endtime` > NOW() ORDER BY (SELECT COUNT(*) FROM `accounts` INNER JOIN `votes` ON `accounts`.`address`=`votes`.`addr` WHERE `type` = 'proposal' AND `pid`= `proposals`.`id`) DESC LIMIT 3";
 $result_acct = $conn->query($sql2);
 if ($result_acct->num_rows > 0) {
 while($row = $result_acct->fetch_assoc()) {
@@ -29,7 +29,7 @@ $conn->query($sql);
 
 
 curl_get($TGBOT.urlencode("Hot Proposal : ".$row['title'] ."
-By User  : '.$rowk[1]."
+By User  : ".$rowk[1]."
 Check it out here : ".$url.'proposal.php?id='.$row['id']));
 $hookObject = json_encode([
     "username" => "Idena.vote",
@@ -79,7 +79,7 @@ curl_close( $ch );
 
 
 
-$sql2 = "SELECT * , (SELECT COUNT(*) FROM `accounts` INNER JOIN `votes` ON `accounts`.`address`=`votes`.`addr` WHERE (`state` = 'Verified' OR `state` = 'Human' OR `state` = 'Newbie') AND `type` = 'poll' AND `pid`= `polls`.`id` )AS 'count' FROM polls WHERE (SELECT COUNT(*) FROM `accounts` INNER JOIN `votes` ON `accounts`.`address`=`votes`.`addr` WHERE `type` = 'poll' AND `pid`= `polls`.`id`) > 4 AND `ann` = '0' AND`endtime` > NOW() ORDER BY (SELECT COUNT(*) FROM `accounts` INNER JOIN `votes` ON `accounts`.`address`=`votes`.`addr` WHERE `type` = 'poll' AND `pid`= `polls`.`id`) DESC LIMIT 50";
+$sql2 = "SELECT * , (SELECT COUNT(*) FROM `accounts` INNER JOIN `votes` ON `accounts`.`address`=`votes`.`addr` WHERE (`state` = 'Verified' OR `state` = 'Human' OR `state` = 'Newbie') AND `type` = 'poll' AND `pid`= `polls`.`id` )AS 'count' FROM polls WHERE (SELECT COUNT(*) FROM `accounts` INNER JOIN `votes` ON `accounts`.`address`=`votes`.`addr` WHERE `type` = 'poll' AND `pid`= `polls`.`id`) > 9 AND `ann` = '0' AND`endtime` > NOW() ORDER BY (SELECT COUNT(*) FROM `accounts` INNER JOIN `votes` ON `accounts`.`address`=`votes`.`addr` WHERE `type` = 'poll' AND `pid`= `polls`.`id`) DESC LIMIT 3";
 $result_acct = $conn->query($sql2);
 if ($result_acct->num_rows > 0) {
 while($row = $result_acct->fetch_assoc()) {
@@ -88,7 +88,7 @@ $rowk = $result->fetch_row();
 $sql = "UPDATE `polls` SET `ann` =  1 WHERE `id` = '".$row['id']."'";
 $conn->query($sql);
 curl_get($TGBOT.urlencode("Hot Poll : ".$row['title'] ."
-By User  : '.$rowk[1]."
+By User  : ".$rowk[1]."
 Check it out here : ".$url.'poll.php?id='.$row['id']));
 $hookObject = json_encode([
     "username" => "Idena.vote",
@@ -128,7 +128,7 @@ curl_close( $ch );
 
 
 
-$sql2 = "SELECT * , (SELECT COUNT(*) FROM `accounts` INNER JOIN `votes` ON `accounts`.`address`=`votes`.`addr` WHERE (`state` = 'Verified' OR `state` = 'Human' OR `state` = 'Newbie') AND `type` = 'fvf' AND `pid`= `fvfs`.`id` )AS 'count' FROM fvfs WHERE (SELECT COUNT(*) FROM `accounts` INNER JOIN `votes` ON `accounts`.`address`=`votes`.`addr` WHERE `type` = 'fvf' AND `pid`= `fvfs`.`id`) > 4 AND `ann` = '0' AND`endtime` > NOW() ORDER BY (SELECT COUNT(*) FROM `accounts` INNER JOIN `votes` ON `accounts`.`address`=`votes`.`addr` WHERE `type` = 'fvf' AND `pid`= `fvfs`.`id`) DESC LIMIT 50";
+$sql2 = "SELECT * , (SELECT COUNT(*) FROM `accounts` INNER JOIN `votes` ON `accounts`.`address`=`votes`.`addr` WHERE (`state` = 'Verified' OR `state` = 'Human' OR `state` = 'Newbie') AND `type` = 'fvf' AND `pid`= `fvfs`.`id` )AS 'count' FROM fvfs WHERE (SELECT COUNT(*) FROM `accounts` INNER JOIN `votes` ON `accounts`.`address`=`votes`.`addr` WHERE `type` = 'fvf' AND `pid`= `fvfs`.`id`) > 9 AND `ann` = '0' AND`endtime` > NOW() ORDER BY (SELECT COUNT(*) FROM `accounts` INNER JOIN `votes` ON `accounts`.`address`=`votes`.`addr` WHERE `type` = 'fvf' AND `pid`= `fvfs`.`id`) DESC LIMIT 3";
 $result_acct = $conn->query($sql2);
 if ($result_acct->num_rows > 0) {
 while($row = $result_acct->fetch_assoc()) {
@@ -137,7 +137,7 @@ $rowk = $result->fetch_row();
 $sql = "UPDATE `fvfs` SET `ann` =  1 WHERE `id` = '".$row['id']."'";
 $conn->query($sql);
 curl_get($TGBOT.urlencode("Hot FvF : ".$row['title'] ."
-By User  : '.$rowk[1]."
+By User  : ".$rowk[1]."
 Check it out here : ".$url.'fvf.php?id='.$row['id']));
 $hookObject = json_encode([
     "username" => "Idena.vote",
@@ -149,7 +149,7 @@ $hookObject = json_encode([
             "url" => $url.'fvf.php?id='.$row['id'],
             "type" => "rich",
             "timestamp" => $row['addtime'],
-            "color" => 10181046,
+            "color" => 9181046,
             "author" => [
                 "name" => 'User : '.$rowk[1],
                 "url" => $url.'profile.php?user='.$rowk[1]

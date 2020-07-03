2262b22c0a5c2108cab704e6cb9ed6d41c6cfd7c
ProjXDev <61373609+ProjXDev@users.noreply.github.com>
2020-07-03 20:06:26 +0200

    update footer

--- partials/footer.php
+++ partials/footer.php
@@ -10,19 +10,21 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js"></script>
 
   <footer style="margin:0px" class="footer">
-      <div class="justify-content-center text-center">
-          <br>
-          <div class="copy" id="donation">
-              <p class="desc" style="line-height: 2em;">
-                      Idena.Vote is maintained by community member <a href="https://github.com/RealMahmoud/" target="_blank">@RealMahmoud.</a>
-                      Please support by donating to
-                      <a href="https://scan.idena.io/address/0x400c4f7822a6c0f81fffcd425f98a92d18ebc567" target="_blank">
-                      <span class="donate">0x400c4f7822a6c0f81fffcd425f98a92d18ebc567</span>
-                      </a>
-              </p>
-          </div>
+    <div class="justify-content-center text-center">
+      <br>
+      <div class="copy" id="donation">
+        <p class="desc" style="line-height: 2em;">
+          Idena.Vote is maintained by community member <a href="https://github.com/RealMahmoud/"
+            target="_blank">@RealMahmoud.</a>
+          Please support by donating to
+          <a href="https://scan.idena.io/address/0x400c4f7822a6c0f81fffcd425f98a92d18ebc567" target="_blank">
+            <span class="donate">0x400c4f7822a6c0f81fffcd425f98a92d18ebc567</span>
+          </a>
+        </p>
       </div>
+    </div>
   </footer>
+
 </body>
 
 </html>
